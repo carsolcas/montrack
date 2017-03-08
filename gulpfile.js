caa01dbfc7508897352d3c1caf27cd9e0cea6bde
Carlos Solsona <carlos.solsona@gmail.com>
2017-03-08 22:07:11 +0100

    Add gulp to the project. Create a task to compile compass

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var compass = require('gulp-compass');
+ 
+gulp.task('compass', function() {
+  gulp.src('./css_src/scss/*.scss')
+    .pipe(compass({
+      css: 'montrack/static/css',
+      sass: 'css_src/scss',
+      image: 'montrack/static/images',
+      javascript: 'montrack/static/images',
+      import_path: 'node_modules/materialize-css/sass'
+    }))
+    .pipe(gulp.dest('montrack/static/css'));
+});
