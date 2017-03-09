b80e515ce231f4e127275f283b1de8af395384b2
Carlos Solsona <carlos.solsona@gmail.com>
2017-03-09 21:06:14 +0100

    Update path for css_src

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp');
 var compass = require('gulp-compass');
  
 gulp.task('compass', function() {
-  gulp.src('./css_src/scss/*.scss')
+  gulp.src('./css_src/*.scss')
     .pipe(compass({
       css: 'montrack/static/css',
-      sass: 'css_src/scss',
+      sass: 'css_src',
       image: 'montrack/static/images',
       javascript: 'montrack/static/images',
       import_path: 'node_modules/materialize-css/sass'
