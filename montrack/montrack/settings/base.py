850098ed32f00feb1560b8624071bcfe652d5723
Carlos Solsona <carlos.solsona@gmail.com>
2017-03-14 21:37:30 +0100

    Fix settings for custom document model

--- montrack/montrack/settings/base.py
+++ montrack/montrack/settings/base.py
@@ -135,7 +135,7 @@ MEDIA_URL = '/media/'
 # Wagtail settings
 
 WAGTAIL_SITE_NAME = "montrack"
-WAGTAILIMAGES_IMAGE_MODEL = 'tracks.Track'
+WAGTAILDOCS_DOCUMENT_MODEL = 'tracks.Track'
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
