c65d83b6adae76ad33e93268b6cad1327e395820
Carlos Solsona <carlos.solsona@gmail.com>
2017-03-19 20:14:49 +0100

    Update css configuration

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var compass = require('gulp-compass');
 gulp.task('compass', function() {
   gulp.src('./css_src/*.scss')
     .pipe(compass({
-      css: 'montrack/static/css',
+      css: 'montrack/montrack/static/css',
       sass: 'css_src',
-      image: 'montrack/static/images',
-      javascript: 'montrack/static/images',
+      image: 'montrack/montrack/static/images',
+      javascript: 'montrack/montrack/static/js',
       import_path: 'node_modules/materialize-css/sass'
     }))
-    .pipe(gulp.dest('montrack/static/css'));
+    .pipe(gulp.dest('montrack/montrack/static/css'));
 });
