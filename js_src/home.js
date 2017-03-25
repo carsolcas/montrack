342655fd68a8ab41ef279a03ee56813df6e15e30
Carlos Solsona <carlos.solsona@gmail.com>
2017-03-25 20:11:12 +0100

    Add masonry to the projecto

--- js_src/home.js
+++ js_src/home.js
@@ -1,3 +1,9 @@
 import 'materialize-css';
+import Masonry from 'masonry-layout';
 
 $('.button-collapse').sideNav();
+
+let msnry = new Masonry( '.grid', {
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
