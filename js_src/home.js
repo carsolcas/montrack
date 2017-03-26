8305c5beaef17afcf73db90b98f7dab567d0861f
Carlos Solsona <carlos.solsona@gmail.com>
2017-03-26 22:03:38 +0200

    Move the content from the base to the home_page tempate

--- js_src/home.js
+++ js_src/home.js
@@ -2,6 +2,7 @@ import 'materialize-css';
 import Masonry from 'masonry-layout';
 
 $('.button-collapse').sideNav();
+$('.parallax').parallax();
 
 let msnry = new Masonry( '.grid', {
   itemSelector: '.grid-item',
