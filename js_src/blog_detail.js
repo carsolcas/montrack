cff195cf72da1978731d0820a580c0e876896c7b
Carlos Solsona <carlos.solsona@gmail.com>
2017-04-02 21:34:26 +0200

    Add toc to blog detail view

--- js_src/blog_detail.js
+++ js_src/blog_detail.js
@@ -1,5 +1,14 @@
 import 'materialize-css';
-import Masonry from 'masonry-layout';
 
 $('.button-collapse').sideNav();
+
 $('.carousel').carousel();
+$('.scrollspy').scrollSpy();
+
+$(document).ready(function(){
+    var topOffset;
+    topOffset = $('nav').height() + $('.parallax-container').height();
+    $('.toc-wrapper').pushpin({
+        top: topOffset,
+    });
+});
