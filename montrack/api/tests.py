6603f022ef687655fd4b09bda08be69fb3078e2e
Carlos Solsona <carlos.solsona@gmail.com>
2017-04-10 19:54:22 +0200

    Add a mock to test the points api

--- montrack/api/tests.py
+++ montrack/api/tests.py
@@ -1,9 +1,12 @@
+from unittest.mock import patch
+
 from django.test import TestCase
-from tracks.models import Track
+from django.core.urlresolvers import reverse
 
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.core.urlresolvers import reverse
+
+from tracks.models import Track
 
 
 class TrackTestCase(TestCase):
@@ -15,10 +18,14 @@ class TrackTestCase(TestCase):
     def test_track_title(self):
         self.assertEqual(self.track.title, self.track_title)
 
-    def test_get_track(self):
+    @patch('tracks.models.Track.get_points')
+    def test_get_track(self, get_points_mock):
+        points = [[1, 2, 3, 3], [-4, 5, 6, 6]]
+        get_points_mock.return_value = points
+
         url = reverse('track_item', kwargs={'pk': self.track.pk})
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.json()
         self.assertEqual(data['id'], self.track.pk)
-        self.assertEqual(data['points'], [])
+        self.assertEqual(data['points'], points)
